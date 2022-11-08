183e64e6c3d69b65922dd63085b3986b16dc0d30
hyoenji.choi <jh04022@naver.com>
2022-11-08 11:59:11 +0900

    업로드 3

--- js/myjs.js
+++ js/myjs.js
@@ -4,16 +4,28 @@ const min = document.querySelector('.min-box');
 const sec = document.querySelector('.sec-box');
 const btnStart = document.querySelector('.start')
 const btnReset = document.querySelector('.reset')
-const hourvalue = hour.value ;
-const minvalue = min.value;
-const secvalue = sec.value;
+function change(){
+  btnStart.style.background= "url('../image/icon-start.png') no-repeat 15px";
+  btnReset.style.background="url('../image/icon-reset.png') no-repeat 15px";
+  btnStart.style.backgroundColor= '#5180FF';
+  btnReset.style.backgroundColor='#FB7099';
+  btnStart.style.color='#fff';
+  btnReset.style.color='#fff';
+}
+hour.addEventListener('input',change);
+min.addEventListener('input',change);
+sec.addEventListener('input',change);
 
 btnStart.addEventListener('click',function(){
  // setTimeout((),parseInt(sec.value)*1000);
- btnStart.style.backgroundColor= '#5180FF';
- btnReset.style.backgroundColor='#FB7099';
- btnStart.style.color='#fff';
- btnReset.style.color='#fff';
+ btnStart.style.background= "url('../image/icon-start.png') no-repeat 15px";
+ btnStart.textContent='PAUSE';
+ btnStart.style.background="url('../image/icon-stop.png') no-repeat 15px";
+ btnStart.style.backgroundColor='#15C2B8';
+
+ if(sec.value>0)
+ sec.value--;
+ 
 })
 btnReset.addEventListener('click',function(){
   hour.value = "00";
